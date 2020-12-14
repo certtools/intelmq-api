6202dfdb9a1dd824a2fb2059ad02746bd830dcca
Sebastian Wagner <wagner@cert.at>
2020-12-14 17:39:15 +0100

    PKG: align version numbers and use ~ for debian changelog

--- intelmq_api/version.py
+++ intelmq_api/version.py
@@ -3,5 +3,5 @@
 SPDX-FileCopyrightText: 2020 Birger Schacht
 SPDX-License-Identifier: AGPL-3.0-or-later
 """
-__version_info__ = (2, 3, 0, 'alpha2')
+__version_info__ = (2, 3, 0, 'alpha3')
 __version__ = '.'.join(map(str, __version_info__))
