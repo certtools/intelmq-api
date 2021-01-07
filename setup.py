8fcce4c72807ea66f98600f4b1540c9647e947d3
Sebastian Wagner <wagner@cert.at>
2021-01-07 21:02:02 +0100

    setup: set minimum intelmq version to 2.3.0a2
    The currently available intelmq version 2.3.0a2 is enough to run
    intelmq-api. Otherwise, intelmq-api is not installable, as the stable
    version intelmq 2.3.0 is not yet released.

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     url="https://github.com/certtools/intelmq-api",
     packages=setuptools.find_packages(),
-	install_requires=[
-		"hug>=2.3.0",
-        "intelmq>=2.3.0",
+    install_requires=[
+        "hug>=2.3.0",
+        "intelmq>=2.3.0a2",
     ],
     python_requires='>=3.6',
-	description="Intelmq-API is a REST API to manage an intelmq, a solution for IT security teams for collecting and processing security feeds",
+    description="Intelmq-API is a REST API to manage an intelmq, a solution for IT security teams for collecting and processing security feeds",
     data_files = [('share/intelmq-api', ['intelmq-api-config.json'])]
 )
