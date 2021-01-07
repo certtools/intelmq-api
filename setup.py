3a3fac0e9d40b20323fe2d1b4cc8865bf7603c08
Bernhard Reiter <bernhard@intevation.de>
2021-01-07 17:28:52 +0100

    Fix code style for setup.py (minor)
     * Remove mixed tabs/spaces from file.
     * While at it anway: make flake8 compatible for consistency reasons
       and adapt description to what is written in the README.

--- setup.py
+++ setup.py
@@ -17,11 +17,13 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     url="https://github.com/certtools/intelmq-api",
     packages=setuptools.find_packages(),
-	install_requires=[
-		"hug>=2.3.0",
+    install_requires=[
+        "hug>=2.3.0",
         "intelmq>=2.3.0",
     ],
     python_requires='>=3.6',
-	description="Intelmq-API is a REST API to manage an intelmq, a solution for IT security teams for collecting and processing security feeds",
-    data_files = [('share/intelmq-api', ['intelmq-api-config.json'])]
+    description="Intelmq-API is a hug based API to intelmq,"
+                " a solution for IT security teams for collecting"
+                " and processing security feeds",
+    data_files=[('share/intelmq-api', ['intelmq-api-config.json'])]
 )
