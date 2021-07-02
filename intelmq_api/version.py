ff1c4796d3d96393d6690d3cd295068e817cb1c6
Sebastian Wagner <wagner@cert.at>
2021-07-02 18:59:40 +0200

    MAINT: Start 3.0.1 development

--- intelmq_api/version.py
+++ intelmq_api/version.py
@@ -3,5 +3,5 @@
 SPDX-FileCopyrightText: 2020-2021 Birger Schacht, Sebastian Wagner
 SPDX-License-Identifier: AGPL-3.0-or-later
 """
-__version_info__ = (3, 0, 0)
+__version_info__ = (3, 0, 1, 'beta1')
 __version__ = '.'.join(map(str, __version_info__))
