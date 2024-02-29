c9cdc67d95ee421802ae5856e9420af0a7278f5b
aaronkaplan <aaron@lo-res.org>
2024-02-29 23:03:35 +0100

    bump version in parallel with the main intelmq version increase in order to not confuse anyone.

--- intelmq_api/version.py
+++ intelmq_api/version.py
@@ -3,5 +3,5 @@
 SPDX-FileCopyrightText: 2020-2023 Birger Schacht, Sebastian Wagner
 SPDX-License-Identifier: AGPL-3.0-or-later
 """
-__version_info__ = (3, 2, 0)
+__version_info__ = (3, 3, 0)
 __version__ = '.'.join(map(str, __version_info__))
